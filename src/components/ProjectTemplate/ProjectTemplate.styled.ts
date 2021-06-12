a401a8f08072377c3641c3fcd433d7080328f185
danivelop <mofg266@gmail.com>
2021-06-12 20:56:33 +0900

    Project페이지에서 모바일일 경우 상단 navigation영역으로 인한 padding값 변경

--- src/components/ProjectTemplate/ProjectTemplate.styled.ts
+++ src/components/ProjectTemplate/ProjectTemplate.styled.ts
@@ -4,4 +4,8 @@ import styled from 'styled-components'
 export const ProjectContainer = styled.div`
   width: 100%;
   padding-top: 144px;
+
+  @media ${({ theme }) => theme.media.mobile} {
+    padding-top: 60px;
+  }
 `
